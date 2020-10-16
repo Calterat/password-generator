7a5fa93f3c02c042e9f97380a05136cac80b2b40
Calterat <Calterat@gmail.com>
2020-10-16 12:33:01 -0500

    added criteria arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,20 @@
 
 
 // Generate password based on criteria
+const generatePassword = () => {
+  // Arrays of criteria type
+  const lowCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  const upCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  const num = ['0','1','2','3','4','5','6','7','8','9']
+  const specChar = [' ','!','"','#','$','%',"'",'(',')','*',',','-','.','/',':',';','>','=','<','?','@','[','\\',']','^','_','`','{','|','}','~']
 // prompt for password criteria
 // - length of password
 // - at least 8 character, no more that 128
 // - character types: lowercase, uppercase, numeric, and/or special characters
 // validate input (at least one character type should be selected)
 // generate password on an alert or written to the page
-
+  return specChar;
+}
 
 
 // Get references to the #generate element
