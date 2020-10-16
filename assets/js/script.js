af33c6388320a44bfc4db9a1afb97505692047fd
Calterat <Calterat@gmail.com>
2020-10-16 13:17:37 -0500

    added prompts and variables to store info

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,33 @@ const generatePassword = () => {
   const upCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
   const num = ['0','1','2','3','4','5','6','7','8','9']
   const specChar = [' ','!','"','#','$','%',"'",'(',')','*',',','-','.','/',':',';','>','=','<','?','@','[','\\',']','^','_','`','{','|','}','~']
-// prompt for password criteria
-// - length of password
-// - at least 8 character, no more that 128
-// - character types: lowercase, uppercase, numeric, and/or special characters
+
+  // Prompt User for Password Criteria
+
+  // - Ask how many characters the password will be and parseInt the value to be used in loops.
+  let passLength = prompt("How long do you want your password? (Minimum of 8 Characters and Maximum of 128)");
+  passLength = parseInt(passLength);
+
+  // - Ask if Lowercase letters are wanted
+  let lowCaseChoice = confirm("Do you want lowercase values?");
+
+  // - Ask if Uppercase letters are wanted
+  let upCaseChoice = confirm("Do you want uppercase values?");
+
+  // - Ask if Numbers are wanted
+  let numChoice = confirm("Do you want number values?");
+
+  // - Ask if Special Characters are wanted
+  let specCharChoice = confirm("Do you want Special Characters?");
+
+  // generate password
+
+
+
+// VALIDATE: at least 8 character, no more that 128
+// Validate user choice included as least one of the boolean experessions
 // validate input (at least one character type should be selected)
-// generate password on an alert or written to the page
-  return specChar;
+  return 'Test';
 }
 
 
